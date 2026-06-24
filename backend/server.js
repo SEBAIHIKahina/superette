399d7ecefab245294e5a414e0fd80a9b7d1b4981
SEBAIHIKahina <kikanina189@gmail.com>
2026-06-24 13:37:56 +0200

    clean backend files

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+// const db = require("./models");
+
+var corsOptions = {
+  origin: "http://localhost:5173"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+//
+
+// db.sequelize.sync({ force: true })
+//   .then(() => {
+//     console.log("Synced db.");
+//   })
+//   .catch((err) => {
+//     console.log("Failed to sync db: " + err.message);
+//   });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "supertte application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
